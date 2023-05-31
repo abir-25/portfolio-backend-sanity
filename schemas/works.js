a539b0cceb03b2fd659f2a7ca1518678a63ed025
abir-25 <abirbgc25@gmail.com>
2023-05-31 07:56:36 +0600

    work section field added

--- schemas/works.js
+++ schemas/works.js
@@ -1,50 +1,67 @@
 export default {
-    name: 'works',
-    title: 'Works',
-    type: 'document',
-    fields: [
-      {
-        name: 'title',
-        title: 'Title',
-        type: 'string',
+  name: 'works',
+  title: 'Works',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'longTitle',
+      title: 'Long Title',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    },
+    {
+      name: 'projectLink',
+      title: 'Project Link',
+      type: 'string',
+    },
+    {
+      name: 'codeLink',
+      title: 'Code Link',
+      type: 'string',
+    },
+    {
+      name: 'imgUrl',
+      title: 'ImageUrl',
+      type: 'image',
+      options: {
+        hotspot: true,
       },
-    
-      {
-        name: 'description',
-        title: 'Description',
-        type: 'string',
-      },
-      {
-        name: 'projectLink',
-        title: 'Project Link',
-        type: 'string',
-      },
-      {
-        name: 'codeLink',
-        title: 'Code Link',
-        type: 'string',
-      },
-      {
-        name: 'imgUrl',
-        title: 'ImageUrl',
-        type: 'image',
-        options: {
-          hotspot: true,
+    },
+    {
+      name: 'completion',
+      title: 'Completion Time',
+      type: 'string',
+    },
+    {
+      name: 'feeBD',
+      title: 'Fee Charged BD',
+      type: 'string',
+    },
+    {
+      name: 'feeNonBD',
+      title: 'Fee Charged Non BD',
+      type: 'string',
+    },
+    {
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      of: [
+        {
+          name: 'tag',
+          title: 'Tag',
+          type: 'string',
         },
-      },
-   
-      {
-        name: 'tags',
-        title: 'Tags',
-       type:'array',
-       of: [
-         {
-           name:'tag',
-           title:'Tag',
-           type:'string'
-         }
-       ]
-      },
-     
-    ],
-  };
+      ],
+    },
+  ],
+}
